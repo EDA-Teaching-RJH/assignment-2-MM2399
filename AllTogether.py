6f8b030e87ed52f770dab4685e2f08d167ca465a
MM2399 <mm2399@kent.ac.uk>
2024-10-27 21:17:49 +0000

    v2

--- AllTogether.py
+++ AllTogether.py
@@ -2,7 +2,11 @@
 import random
 
 def main():
-    print (remove_even_numbers(create_list(10)))
+    list = create_list(10)
+    #using both a for and while loop to remove the even numbers.
+    #originally used a for loop but saw a request of using a while loop in the question so just kept both.
+    print (remove_even_numbers(list))
+    print (remove_even_numbers_2(list))
  
 #creates the list of random numbers 
 def create_list(length):
@@ -11,7 +15,7 @@ def create_list(length):
         list.append(random.randint(1,100))
     return list
 
-#removes even numbers from the list
+#removes even numbers from the list using FOR loop
 def remove_even_numbers(list):
     removed_even = []
     #used a for loop instead of while loop
@@ -20,4 +24,18 @@ def remove_even_numbers(list):
             removed_even.append(list[_])
     return removed_even
 
+#removes even numbers from the list using WHILE loop
+def remove_even_numbers_2(list):
+    removed_even = []
+    count = 0
+    #uses while loop as requested
+    while True:
+        if (count) == len(list):
+            return removed_even
+        else:
+            if list[count] % 2 != 0:
+                removed_even.append(list[count])
+        count += 1
+            
+        
 main()
